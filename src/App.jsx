767aae651039a0dbf52252d6ee3e201781d8e9db
annams <annaextrasteckel@gmail.com>
2025-03-30 19:50:39 -0700

    route and routes imported and set up in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,36 @@ import MailboxForm from "./components/MailboxForm/MailboxForm.jsx";
 // importing but not using quite yet
 import MailboxDetails from "./components/MailboxDetails/MailboxDetails.jsx";
 
+import { Route, Routes } from 'react-router';
+
 const App = () => {
     return (
         <>
             <Navbar />
-            <h1>Mail Room</h1>
+
+            <Routes>
+                <Route 
+                    path='/'
+                    element={ <main><h1>Post Office</h1></main> }
+                />
+                
+                <Route
+                    path='/mailboxes'
+                    element={ < MailboxList /> }
+                />
+
+                <Route 
+                    path='/new-mailbox'
+                    element={ <MailboxForm />}
+                />
+
+                <Route
+                    path='/mailboxes/:mailboxId'
+                    element={<MailboxDetails />}
+                />
+
+            </Routes>
+
             <MailboxList />
         </>
     );
