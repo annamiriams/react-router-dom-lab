17e366136c38075f71096ada09d34550a2844ed9
annams <annaextrasteckel@gmail.com>
2025-03-30 19:41:43 -0700

    mailbox details barebones added and imported to app.jsx but not using in ui yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import MailboxList from "./components/MailboxList/MailboxList.jsx";
 // importing but not using quite yet
 import MailboxForm from "./components/MailboxForm/MailboxForm.jsx";
+// importing but not using quite yet
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails.jsx";
 
 const App = () => {
     return (
--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,1 +1,11 @@
 // src/components/MailboxDetails/MailboxDetails.jsx
+
+const MailboxDetails = () => {
+    return (
+        <>
+            <p>Mailbox details will go here.</p>
+        </>
+    );
+};
+
+export default MailboxDetails;
