1af3e7c1e20c74d1046f68a953a938fb45a35d6a
annams <annaextrasteckel@gmail.com>
2025-03-30 21:44:19 -0700

    mailbox details ui set up

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
                 
                 <Route
                     path='/mailboxes'
-                    element={ <MailboxList /> }
+                    element={ <MailboxList mailboxes={mailboxes}/> }
                 />
 
                 <Route 
--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,11 +1,39 @@
 // src/components/MailboxDetails/MailboxDetails.jsx
 
-const MailboxDetails = () => {
+import { useParams } from "react-router";
+
+// useParams() hook will be helpful
+// use value extracted from URL params to find() a specific mailbox in mailboxes state 
+
+const MailboxDetails = (props) => {
+    // useParams allows you to use access parameters from the current route
+    // destructure mailboxId from the object returned by useParams()
+    // for example: if the current route is /mailbox/5 (from /mailbox/:mailboxId), useParams() will return {mailboxId: '5'}, so mailboxId would be set to '5'
+    const { mailboxId } = useParams();
+
+    // props.mailboxes is the list of mailboxes passed as a prop to the component 
+    const selectedBox = props.mailboxes.find(
+        // the callback function takes each mailbox and checks if the _id matches mailboxId (once it has been converted from a string to a Number) 
+        (mailbox) => mailbox._id === Number(mailboxId)
+    );
+
+    if (!props.selectedBox) {
+        return (
+            <>
+                <p>Mailbox not found!</p>
+            </>
+        )
+    }
+    
     return (
         <>
-            <p>Mailbox details will go here.</p>
+            <p>Box Number: {props.selectedBox._id}</p>
+            <p>Box Owner: {props.selectedBox.boxOwner}</p>
+            <p>Box Size: {props.selectedBox.boxSize}</p>
         </>
     );
-};
+}
+
+
 
 export default MailboxDetails;
